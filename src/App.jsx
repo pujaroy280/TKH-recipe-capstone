79e727bde82da8401d283468a9e308122797a237
Danny Castle <dannycastle@Dannys-MacBook-Pro.local>
2024-04-27 17:36:04 -0400

    working on connecting the frontend. Saving progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxidth='lg'>
+      <AppBar position='static' color='inherit'>
+          <Typography variant='h2' align='center'>Recipe App</Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+            <Grid container justifyContent='space-between' alignItems='stretch' spacing={4}>
+                <Grid item xs={12} sm={7}>
+                    <Posts/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Form/>
+                </Grid>
+            </Grid>
+        </Container>
+      </Grow>
+      </Container>
   )
 }
-
 export default App
---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,14 @@
+import Post from "./Post/Post"
+
+
+const Posts = () => {
+  return (
+    <div>
+        <Post/>
+        <Post/>
+      <h1>Posts</h1>
+    </div>
+  )
+}
+
+export default Posts
