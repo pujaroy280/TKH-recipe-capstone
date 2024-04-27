d64f1e1b70860e31eafedcee032d4afb9cc7a76d
Danny Castle <dannycastle@Dannys-MacBook-Pro.local>
2024-04-27 16:18:50 -0400

    server is running

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://capstone:2yjYAtbiS2udjGDR@cluster0.grwezmb.mongodb.net/';
+
+const PORT = 3000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(()=> app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)))
+    .catch((error)=> console.log(error.message));
+
